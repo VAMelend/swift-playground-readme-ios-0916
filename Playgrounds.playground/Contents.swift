f2866a6d1ddb9bd6bf965856a340ed9ba9ee57d8
Victoria Melendez <Victoria@Victorias-MacBook-Pro.local>
2016-08-29 18:02:50 -0400

    done

--- Playgrounds.playground/Contents.swift
+++ Playgrounds.playground/Contents.swift
@@ -1,2 +1,13 @@
 var str = "Hello, world"
 str = "Goodbye, world"
+var favoriteWord = "lumberjack"
+var favoriteColor = "orange"
+var favoriteMusician = "Wilco"
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
